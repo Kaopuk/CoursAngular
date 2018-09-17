1f823775821b67106d2dff5400985dbcdd894f85
Kaopuk <baptiste.lavignon@gmail.com>
2018-09-17 15:41:28 +0200

    integration du routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,45 +6,10 @@ import {AppareilServices} from './services/appareil.services';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
+export class AppComponent {
+  constructor() {
 
-  lastUpdate = new Promise(
-    (resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-  appareils: any[];
-
-  constructor(private appareilServices: AppareilServices) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  ngOnInit() {
-    this.appareils = this.appareilServices.appareils;
-  }
-
-  onAllumer() {
-  this.appareilServices.switchOnAll();
-  }
-
-  onEteindre() {
-    this.appareilServices.switchOffAll();
-
-  }
-
-
-
-
+ }
 }
 
 
