64f3c207c93256602e12e3ac389be390e2cc9190
Kaopuk <baptiste.lavignon@gmail.com>
2018-09-18 16:06:31 +0200

    Ajout d'un observable et d'une souscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {AppareilServices} from './services/appareil.services';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit, OnDestroy {
 
- }
+  secondes: number;
+  counterSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.counterSubscription.unsubscribe();
+  }
 }
 
 
