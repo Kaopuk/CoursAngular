eb688b7da3d49526d44f80726def1901e33e385d
Kaopuk <baptiste.lavignon@gmail.com>
2018-09-19 15:05:45 +0200

    formulaire par methode template et reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { MonPremierComponent } from './mon-premier/mon-premier.component';
-import { AppareilComponent } from './appareil/appareil.component';
+import {AppComponent} from './app.component';
+import {MonPremierComponent} from './mon-premier/mon-premier.component';
+import {AppareilComponent} from './appareil/appareil.component';
 
 import {AppareilServices} from './services/appareil.services';
-import { AuthComponent } from './auth/auth.component';
-import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import {AuthComponent} from './auth/auth.component';
+import {AppareilViewComponent} from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthService} from './services/auth.service';
-import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
-import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import {SingleAppareilComponent} from './single-appareil/single-appareil.component';
+import {FourOhFourComponent} from './four-oh-four/four-oh-four.component';
 import {AuthGuard} from './services/auth-guard.service';
-import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import {EditAppareilComponent} from './edit-appareil/edit-appareil.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {User} from './models/user.model';
+import {UserServices} from './services/user.services';
+import {buildPath} from 'selenium-webdriver/http';
+import {NewUserComponent} from './new-user/new-user.component';
 
 const appRoutes: Routes = [
-  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
-  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent},
-  { path: 'edit', canActivate: [AuthGuard], component : EditAppareilComponent},
-  { path: 'auth', component: AuthComponent },
-  { path: '', component: AppareilViewComponent },
-  { path: 'not-found', component: FourOhFourComponent },
-  { path: '**', redirectTo: '/not-found' }
+  {path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  {path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+  {path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
+  {path: 'auth', component: AuthComponent },
+  {path: 'users', component: UserListComponent },
+  {path: 'new-user', component: NewUserComponent },
+  {path: '', component: AppareilViewComponent },
+  {path: 'not-found', component: FourOhFourComponent },
+  {path: '**', redirectTo: '/not-found' }
 
 ];
 
@@ -36,19 +43,24 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent
+    EditAppareilComponent,
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-  AppareilServices ,
-  AuthService,
-  AuthGuard
+    AppareilServices,
+    AuthService,
+    AuthGuard,
+    UserServices
   ],
   bootstrap: [AppComponent]
 
 })
-export class AppModule { }
+export class AppModule {
+}
